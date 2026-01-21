b1d7c372d40f9c92a2f2cf5fc93b0e5d53ca707e
Luiz <luizzlh2004@gmail.com>
2026-01-20 21:55:25 -0300

    Adicionando classe Service e criando método de agendamento na classe Service.

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,8 +1,10 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultas;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("consultas")
 public class ConsultaController {
+
+    @Autowired
+    private AgendaDeConsultas agendaDeConsultas;
 
     @PostMapping
     @Transactional
